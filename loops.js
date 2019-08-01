a601c50c12803249469f7e9d8bd520a64e940bc7
Malik Bouzid <malik.amazigh78@gmail.com>
2019-08-01 23:51:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,28 @@
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) { array.push("I am 1 strange loop.");}
+    else {array.push(`I am ${i} strange loops.`)}
   }
   return array;
 }
 
  function whileLoop(n) {
-  let countdown = n;
-
-     while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
+  let countdown= n;
+  while (countdown > 0) {
+    console.log(--countdown)
   }
-
- function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+  return "done"
 }
+
+ function doWhileLoop(num) {
+  var i = 0; 
+  function incrementVariable () {
+    i= i+1; 
+    return i; 
+  }
+
+   do{ 
+    console.log("I run once regardless"); }
+
+     while (incrementVariable() < num );
+  }
